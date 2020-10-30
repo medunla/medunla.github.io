1675f381eae10201ad506115c191ed8af2bc0f67
PanupatK <panupatnew@gmail.com>
2020-10-30 22:24:34 +0700

    deploy

---
+++ precache-manifest.37945b4eeb707d83911f058265a36eba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab84289a7f259aea051",
+    "url": "/css/app.52cdeb9a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bef71df44a805d74f82740a8dc238e95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af19110f439adce6168a",
+    "url": "/js/about.c4e57910.js"
+  },
+  {
+    "revision": "1ab84289a7f259aea051",
+    "url": "/js/app.26e9ddb2.js"
+  },
+  {
+    "revision": "4124572199d4b331790b",
+    "url": "/js/chunk-vendors.7295c9df.js"
+  },
+  {
+    "revision": "d49cf6b1baa26ed7d400b042944ed7d0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
