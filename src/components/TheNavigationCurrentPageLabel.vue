c86aec768e659693c7d8be604f019d71a1fc88ef
PanupatK <panupatnew@gmail.com>
2021-06-09 23:55:13 +0700

    Adjust navigation page label to show on portfolio detail, about, contact pages

--- src/components/TheNavigationCurrentPageLabel.vue
+++ src/components/TheNavigationCurrentPageLabel.vue
@@ -1,15 +1,23 @@
 <template>
-	<div
-		v-if="pageLabel"
-		class="navigation-page-current is-show"
-	>
-		<span class="button-navigation-page-current">{{ pageLabel }}</span>
-	</div>
+	<transition name="fade">
+		<div
+			v-if="isShow && pageLabel"
+			class="navigation-page-current"
+		>
+			<span class="button-navigation-page-current">{{ pageLabel }}</span>
+		</div>
+	</transition>
 </template>
 
 <script>
 import { mapState } from "vuex";
-import { NAVIGATION_PORTFOLIO_DETAIL } from "../assets/js/constant";
+import { NAVIGATION_PORTFOLIO_DETAIL, NAVIGATION_ABOUT, NAVIGATION_CONTACT } from "../assets/js/constant";
+
+const enableRoutesName = [
+	NAVIGATION_PORTFOLIO_DETAIL.name,
+	NAVIGATION_ABOUT.name,
+	NAVIGATION_CONTACT.name
+]
 
 export default {
 	name: "TheNavigationCurrentPageLabel",
@@ -17,6 +25,9 @@ export default {
 		...mapState({
 			portfolioDetailData: (state) => state.portfolioDetail
 		}),
+		isShow() {
+			return enableRoutesName.includes(this.$route.name);
+		},
 		pageLabel() {
 			if (this.$route.name === NAVIGATION_PORTFOLIO_DETAIL.name) {
 				return this.portfolioDetailData.name;
