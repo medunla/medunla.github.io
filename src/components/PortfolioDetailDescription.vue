fb4de01a15c32e82e1ce11b96a5a80cdc14d3ad0
PanupatK <panupatnew@gmail.com>
2021-06-09 22:06:09 +0700

    Adjust style for portfolio detail

--- src/components/PortfolioDetailDescription.vue
+++ src/components/PortfolioDetailDescription.vue
@@ -1,20 +1,21 @@
 <template>
 	<div class="portfolio-details">
-		<h2 class="title">{{ portfolio.name }}</h2>
-		<ul class="categorise">
+		<h2 class="portfolio-details-title">{{ portfolio.name }}</h2>
+		<ul class="portfolio-details-categorise">
 			<li
 				v-for="category in portfolio.categories"
 				:key="category.name"
+				class="portfolio-details-categorise-item"
 			>
 				{{ category.name }}
 			</li>
 		</ul>
-		<div class="description">
+		<div class="portfolio-details-description">
 			<p v-if="portfolio.description">{{ portfolio.description }}</p>
-			<div class="wrapper">
-				<p v-if="portfolio.client"><b class="label">Client: </b> {{ portfolio.client }}</p>
+			<div class="portfolio-details-description-wrapper">
+				<p v-if="portfolio.client"><b class="portfolio-details-description-label">Client: </b> {{ portfolio.client }}</p>
 				<p v-if="portfolio.demos.length > 0">
-					<b class="label">Demo: </b>
+					<b class="portfolio-details-description-label">Demo: </b>
 					<span
 						v-for="(demo, index) in portfolio.demos"
 						:key="index"
@@ -30,7 +31,7 @@
 					</span>
 				</p>
 				<p v-if="portfolio.app_stores.app_store">
-					<b class="label">App store: </b>
+					<b class="portfolio-details-description-label">App store: </b>
 					<a
 						:href="portfolio.app_stores.app_store"
 						:title="portfolio.app_stores.app_store"
@@ -40,7 +41,7 @@
 					</a>
 				</p>
 				<p v-if="portfolio.app_stores.play_store">
-					<b class="label">Play store: </b>
+					<b class="portfolio-details-description-label">Play store: </b>
 					<a
 						:href="portfolio.app_stores.play_store"
 						:title="portfolio.app_stores.play_store"
